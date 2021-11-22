a6ef800f4477fce551e4dc3b06b5d7d213ee0953
JayabharathiJayaraman <jayabharathi.jayaraman@iths.se>
2021-11-22 15:24:21 +0100

    Merge conflicts resolved

--- src/screens/CreateProductScreen.tsx
+++ src/screens/CreateProductScreen.tsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   View,
@@ -15,6 +14,13 @@ import ModalPicker from "../components/ModalPicker";
 import { TextInput, Button } from "@react-native-material/core";
 import { StackScreens } from "../helpers/types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import ErrorMessage from "../components/ErrorMessage";
+import { tokens } from "../helpers/translations/appStrings";
+import { translate } from "../helpers/translations/translationConfig";
+import ProductProvider, {
+  IProducts,
+  ProductContext,
+} from "../context/ProductContext";
 
 const CreateProductScreen: React.FC<
   NativeStackScreenProps<StackScreens, "CreateProductScreen">
@@ -22,30 +28,29 @@ const CreateProductScreen: React.FC<
   const [productName, setProductName] = useState<string>("");
   const [productPrice, setProductPrice] = useState<string | "">("");
   const priceNumber: number = parseFloat(productPrice);
-  const [selectedProductType, setSelectedProductType] =
-    useState<string>("Choose Type...");
-  //const {showErrorVisible,toggleErrorVisible} = useToggleErrorVisible();
-  //const [isModalVisible,setIsModalVisible] = useState(false);
+  const [selectedProductType, setSelectedProductType] = useState<string>(
+    "Choose Product Type..."
+  );
   const { showModalVisible, toggleModalVisible } = useToggleModalVisible();
-=======
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, Text, SafeAreaView, TextInput,TouchableOpacity,Modal,Button, Alert } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'
-import useToggleModalVisible from '../customhooks/useToggleModalVisible'
-import ModalPicker from '../components/ModalPicker'
-import ErrorMessage from '../components/ErrorMessage'
-import { tokens } from '../helpers/translations/appStrings'
-import { translate } from '../helpers/translations/translationConfig'
-import ProductProvider, { IProducts, ProductContext } from "../context/ProductContext";
->>>>>>> btnValidation_GV
+  const [saveDisabled, setSaveDisabled] = useState(false);
+
+  const [productsList, setProductsList] = React.useState<IProducts[]>([]);
+  const appContext = React.useContext(ProductContext);
 
   const setModalData = (option: string) => {
     setSelectedProductType(option);
   };
 
-<<<<<<< HEAD
+  useEffect(() => {
+    setSaveDisabled(
+      productName.length === 0 ||
+        productPrice.length === 0 ||
+        selectedProductType == "Choose Product Type..."
+    );
+  }, [productName, productPrice, selectedProductType]);
+
   const validatePrice = () => {
-    console.log("Inside validate Price", priceNumber, selectedProductType);
+    // console.log("Inside validate Price",priceNumber,selectedProductType);
     if (selectedProductType == "Peripheral" && priceNumber < 0) {
       Alert.alert("Error", "Please enter Price > 0 dollars", [
         {
@@ -69,14 +74,36 @@ import ProductProvider, { IProducts, ProductContext } from "../context/ProductCo
         { text: "OK", onPress: () => console.log("OK Pressed") },
       ]);
     }
+
+    // <ErrorMessage setPriceValue={priceNumber} selectedProductType={selectedProductType} />
+  };
+
+  const saveProducts = () => {
+    //appContext?.addProduct(productName,priceNumber,selectedProductType);
+
+    appContext?.productsList?.map(
+      (product, i) => (
+        (product.productName = productName),
+        (product.productPrice = priceNumber),
+        (product.productType = selectedProductType)
+
+        //setProductsList([...productsList,setProductsList])
+      )
+    );
+
+    // appContext?.saveProduct(productsList)
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.headerStyle}>Create New Product</Text>
+      <Text style={styles.headerStyle}>
+        {translate(tokens.screens.AddProductScreen.mainText)}
+      </Text>
       <View style={styles.inputContainer}>
         <TextInput
-          label="Product Name"
+          label={translate(
+            tokens.screens.AddProductScreen.productNameLabelText
+          )}
           style={styles.inputStyle}
           defaultValue={productName}
           onChangeText={text => setProductName(text)}
@@ -85,13 +112,20 @@ import ProductProvider, { IProducts, ProductContext } from "../context/ProductCo
       </View>
       <View style={styles.inputContainer}>
         <TextInput
-          label="Product Price"
+          label={translate(
+            tokens.screens.AddProductScreen.productPriceLabelText
+          )}
           style={styles.inputStyle}
           // defaultValue={productPrice}
           keyboardType="decimal-pad"
           onChangeText={text => setProductPrice(text)}
         />
         <MaterialIcons style={styles.icon} name="money" size={30} />
+      </View>
+      <View>
+        <Text>
+          {translate(tokens.screens.AddProductScreen.productTypeLabelText)}
+        </Text>
       </View>
       <View>
         <TouchableOpacity
@@ -114,31 +148,41 @@ import ProductProvider, { IProducts, ProductContext } from "../context/ProductCo
           <ModalPicker
             toggleModalVisibility={toggleModalVisible}
             setModalData={setModalData}
-            setPriceValue={priceNumber}
           />
         </Modal>
       </View>
+
       <View>
         <Button
+          title={translate(tokens.screens.AddProductScreen.saveButtonText)}
+          color={saveDisabled ? "grey" : "#8a2be2"}
           style={styles.btnStyleSave}
-          title="SAVE"
+          disabled={saveDisabled}
           onPress={() => {
             validatePrice();
+            saveProducts();
           }}
         ></Button>
         <Feather
           style={styles.btwSaveIcon}
           name="download"
           size={22}
-          color="black"
+          color={saveDisabled ? "black" : "blue"}
         />
+        {/*  <MaterialIcons
+          style={styles.btwSaveIcon}
+          name="file-download"
+          size={30}
+          color={saveDisabled ? "grey" : "blue"}
+        /> */}
       </View>
       <View>
         <Button
+          title={translate(tokens.screens.AddProductScreen.cancelButtonText)}
+          color="white"
           style={styles.btnStyleCancel}
-          title="Cancel"
           onPress={() => {
-            props.navigation.navigate("ProductListScreen");
+            props.navigation.navigate("DisplayProductScreen");
           }}
         ></Button>
         <Foundation
@@ -151,157 +195,10 @@ import ProductProvider, { IProducts, ProductContext } from "../context/ProductCo
     </SafeAreaView>
   );
 };
-=======
-const CreateProductScreen : React.FC = () => {
-    const [productName,setProductName] = useState<string>("");
-    const [productPrice,setProductPrice] = useState<string|''>("");
-    const priceNumber : number = parseFloat(productPrice);
-    const [selectedProductType, setSelectedProductType] = useState<string>('Choose Type...');
-    const { showModalVisible, toggleModalVisible } = useToggleModalVisible();
-    const [saveDisabled,setSaveDisabled] = useState(false);
-
-    const [productsList, setProductsList] = React.useState<IProducts[]>([])
-    const appContext = React.useContext(ProductContext);
-
-
-    const setModalData = (option:string)=>{
-        setSelectedProductType(option)
-    }
-
-    useEffect(() => {
-      setSaveDisabled(productName.length === 0 || productPrice.length === 0 || selectedProductType == 'Choose Type...');
-    
-    }, [productName,productPrice,selectedProductType])
-
-    const validatePrice = () => {
-        // console.log("Inside validate Price",priceNumber,selectedProductType);
-        if(selectedProductType == 'Peripheral' && priceNumber < 0){
-            Alert.alert("Error",
-            "Please enter Price > 0 dollars",
-            [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-              },
-              { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-          );
-        }
-          if(selectedProductType == 'Integrated' && (priceNumber > 2600 || priceNumber < 1000)){
-            Alert.alert("Error",
-            "Please enter Price between 1000 and 2600 dollars",
-            [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-              },
-              { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-          );
-
-        } 
-
-        // <ErrorMessage setPriceValue={priceNumber} selectedProductType={selectedProductType} />   
-      }
-
-    const saveProducts = () => {
-      //appContext?.addProduct(productName,priceNumber,selectedProductType);
-
-      appContext?.productsList?.map((product,i) => (
-        
-        product.productName = productName,
-        product.productPrice = priceNumber,
-        product.productType = selectedProductType
-
-        setProductsList([...productsList,setProductsList])
-        
-        ))
-
-        appContext?.saveProduct(productsList)
-
-      
-    }  
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.headerStyle}>{translate(tokens.screens.AddProductScreen.mainText)}</Text>
-            
-            <View style={styles.titletext}>
-                <Text>{translate(tokens.screens.AddProductScreen.productNameLabelText)}</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput 
-                style={styles.inputStyle}
-                defaultValue={productName}
-                onChangeText={(text)=>{
-                  setProductName(text)
-                  }} />
-                <MaterialIcons style={styles.icon} name="add-to-queue" size={20} />
-            </View>
-            <View style={styles.titletext}>
-                <Text>{translate(tokens.screens.AddProductScreen.productPriceLabelText)}</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput style={styles.inputStyle}
-                // defaultValue={productPrice}
-                keyboardType="decimal-pad"
-                onChangeText={(text)=>setProductPrice(text)} />
-                <MaterialIcons style={styles.icon} name="money" size={20} />
-            </View>
-            <View style={styles.titletext}>
-                <Text>{translate(tokens.screens.AddProductScreen.productTypeLabelText)}</Text>
-            </View>
-            <View>
-                <TouchableOpacity style={styles.pickerStyle}
-                onPress={()=>toggleModalVisible(true)}>
-                    <Text style={styles.pickerText}>{selectedProductType}</Text>
-                    <MaterialIcons style={styles.dropDownStyle} name='arrow-drop-down' size={30} />
-                </TouchableOpacity>
-                <Modal 
-                transparent={true}
-                animationType='fade'
-                visible={showModalVisible}
-                onRequestClose={()=>toggleModalVisible(false)}>
-                    
-                    <ModalPicker 
-                    toggleModalVisibility={toggleModalVisible}
-                    setModalData={setModalData}
-                    
-                    />
-                </Modal>
-           </View>
-           
-           <View style={styles.saveBtnStyle} >
-                <Button 
-                  title={translate(tokens.screens.AddProductScreen.saveButtonText)} 
-                  color={saveDisabled ? "grey" : 'blue'} 
-                  disabled={saveDisabled}
-                  onPress={()=>{
-                    validatePrice();
-                    saveProducts();
-                }}></Button>
-                <MaterialIcons name="file-download" size={30} color={saveDisabled ? "grey" : 'blue'} />
-            </View>
-            <View style={styles.cancelBtnStyle}>
-                <Button  
-                title={translate(tokens.screens.AddProductScreen.cancelButtonText)} 
-                color="white" 
-                onPress={()=>{
-                        console.log("Cancel clicked");
-                    }}></Button>
-                <MaterialIcons name="cancel" size={30} color="white" />
-            </View>
-        </SafeAreaView>
-    )
-}
->>>>>>> btnValidation_GV
 
 export default CreateProductScreen;
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -323,16 +220,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
     marginLeft: -120,
-    width: 90,
+    width: 100,
   },
   btnStyleCancel: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignSelf: "center",
+    backgroundColor: "#8a2be2",
     borderWidth: 1,
     borderRadius: 10,
-    marginTop: -36,
-    marginLeft: 78,
+    marginTop: -38,
+    marginLeft: 90,
     width: 105,
   },
   inputStyle: {
@@ -373,111 +271,15 @@ const styles = StyleSheet.create({
   },
   btwSaveIcon: {
     position: "absolute",
-    right: 22,
+    right: 15,
     top: 14,
   },
   btwCancelIcon: {
     position: "absolute",
     right: 7,
-    top: -29,
+    top: -31,
   },
   textStyle: {
     fontSize: 20,
   },
 });
-=======
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-      },
-      headerStyle:{
-        textAlign: 'center', 
-        fontWeight: 'bold',
-        fontSize: 20,
-        marginTop: 20,
-        width: 200,
-      },
-      titletext:{
-        fontWeight:'bold',
-        fontSize:18,
-        margin:5,
-        textAlign:'left'
-      },
-      saveBtnStyle:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignSelf:"center",
-        borderWidth:1,
-        borderRadius:10,
-        borderColor:'blue',
-        width:'80%',
-        color:'white',
-        margin:10,
-        textAlign:'center'
-  },
-  cancelBtnStyle:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignSelf:"center",
-    borderWidth:1,
-    borderRadius:10,
-    borderColor:'blue',
-    width:'80%',
-    backgroundColor:'blue',
-    color:'white',
-    textAlign:'center'
-},
-      inputStyle:{
-        flex:1,
-        backgroundColor:'white',
-        fontSize:25,
-        borderColor:'black',
-        borderWidth:2,
-        height:50,
-        margin:5,
-        borderRadius:15,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        borderColor: '#000',
-        paddingBottom: 10,
-      },
-      pickerStyle:{
-          paddingHorizontal:10,
-          alignSelf:'stretch',
-          borderRadius:10,
-          borderWidth:1,
-          flexDirection:'row',
-          borderColor:'black',
-          justifyContent:'space-between'
-      },
-      pickerText:{
-          marginVertical:10,
-          fontSize:18
-      },
-      dropDownStyle:{
-          color:'black',
-          marginTop:8
-
-      },
-      icon:{
-        position: 'absolute',
-        right: 10,
-        top:10
-      },
-      buttonStyle:{ 
-        borderRadius:5, 
-        paddingVertical:10, 
-        paddingHorizontal:30, 
-        flexDirection: 'row', 
-        backgroundColor: '#3578E5' 
-      },
-      buttonText:{ 
-        marginLeft: 10, 
-        color: '#fff', 
-        fontWeight: 'bold' 
-      }
-    })
-
->>>>>>> btnValidation_GV
--- src/screens/DisplayProductScreen.tsx
+++ src/screens/DisplayProductScreen.tsx
@@ -1,40 +1,75 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, View, Text, SafeAreaView, TextInput,TouchableOpacity,Modal,Button, Alert } from 'react-native'
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+  Button,
+  Alert,
+} from "react-native";
+import { FAB } from "react-native-paper";
 import ProductProvider, { ProductContext } from "../context/ProductContext";
+import { StackScreens } from "../helpers/types";
 
-const DisplayProductScreen : React.FC = (props:any) => {
-    //const { productsList,saveProduct,updateProduct } = useContext(ProductContext);
-    const appContext = useContext(ProductContext);
-return(
-<View>
-    <View>
-    <Button
-    title="Add"
-    onPress={() => {
-      props.navigation.navigate("CreateProductScreen");
-    }}
-  />
-</View>
-<View>
-    <Text>{appContext?.productsList?.map((productsList,i) => (
-          <>
-          <Text key={i}>{productsList.productName}</Text>
-          <Text key={i}>{productsList.productPrice}</Text>
-          <Text key={i}>{productsList.productType}</Text>
-          </>
-        ))}</Text>
-</View>
-</View>
+const DisplayProductScreen: React.FC<
+  NativeStackScreenProps<StackScreens, "DisplayProductScreen">
+> = props => {
+  //const { productsList,saveProduct,updateProduct } = useContext(ProductContext);
+  const appContext = useContext(ProductContext);
+  return (
+    <>
+      <View>
+        <Text>
+          {appContext?.productsList?.map((productsList, i) => (
+            <>
+              <Text key={i}>{productsList.productName}</Text>
+              <Text key={i}>{productsList.productPrice}</Text>
+              <Text key={i}>{productsList.productType}</Text>
+            </>
+          ))}
+        </Text>
+      </View>
 
-)
-}
+      <View>
+        <Text style={styles.textStyle}>Product Lists</Text>
+      </View>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => {
+          props.navigation.navigate("CreateProductScreen");
+        }}
+      />
+    </>
+  );
+};
 
-export default DisplayProductScreen
+export default DisplayProductScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-      },
-    })
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  fab: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    height: 70,
+    backgroundColor: "#8a2be2",
+    borderRadius: 100,
+  },
+  textStyle: {
+    fontSize: 25,
+  },
+});
