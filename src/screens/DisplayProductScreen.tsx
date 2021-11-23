94174e8d65d19dba1e569e5efb8cdd728cc15734
zakariamosa <zakariamosa1211@gmail.com>
2021-11-23 10:13:36 +0100

    added the product in flatlist

--- src/screens/DisplayProductScreen.tsx
+++ src/screens/DisplayProductScreen.tsx
@@ -10,10 +10,12 @@ import {
   Modal,
   Button,
   Alert,
+  FlatList,
 } from "react-native";
 import { FAB } from "react-native-paper";
 import ProductProvider, { ProductContext } from "../context/ProductContext";
 import { StackScreens } from "../helpers/types";
+import { Feather } from '@expo/vector-icons';
 
 const DisplayProductScreen: React.FC<
   NativeStackScreenProps<StackScreens, "DisplayProductScreen">
@@ -23,18 +25,40 @@ const DisplayProductScreen: React.FC<
   return (
     <>
       <View>
-        <Text>
+        {/* <Text>
           {appContext?.productsList?.map((products, index) => (
             <>
               <Text key={index}>{products.productName},{products.productPrice},{products.productType}</Text>
             </>
           ))}
-        </Text>
+        </Text> */}
+
+
+        <FlatList
+        data={appContext?.productsList}
+        keyExtractor={(product) => product.productName}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              //onPress={() => navigation.navigate('Show', { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.productName} - {item.productType} - {item.productPrice}
+                </Text>
+                <TouchableOpacity /*onPress={() => deleteProductPost(item.id)}*/>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+
+
+
       </View>
 
-      <View>
-        <Text style={styles.textStyle}>Product Lists</Text>
-      </View>
       <FAB
         style={styles.fab}
         icon="plus"
@@ -69,5 +93,19 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     fontSize: 25,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
   },
 });
